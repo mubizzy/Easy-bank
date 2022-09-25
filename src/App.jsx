92f5287d22ee3679907833bcf084192b6f54fe55
unknown <ajibolamubarak304@gmail.com>
2022-09-25 07:19:25 +0100

    App

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import Nav from "./pages/nav";
 import Home from "./pages/Home";
 import Content from "./pages/Content";
+import Distro from "./pages/Distro";
 
 function App() {
   return (
     <>
       <Nav />
       <Home />
-      <Content></Content>
+      <Content />
+      <Distro />
     </>
   );
 }

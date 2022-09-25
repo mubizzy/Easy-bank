ab40eafc1bef552e86d63da31b4a2cbafc3f5ae1
unknown <ajibolamubarak304@gmail.com>
2022-09-25 05:16:13 +0100

    App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import Nav from "../src/pages/nav";
+import Nav from "./pages/nav";
+import Home from "./pages/Home";
+import Content from "./pages/Content";
 
 function App() {
   return (
     <>
       <Nav />
+      <Home />
+      {/* <Content></Content> */}
     </>
   );
 }

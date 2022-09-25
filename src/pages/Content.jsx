c42269f2f02d1421cb97c85737dd3ce9481663f2
unknown <ajibolamubarak304@gmail.com>
2022-09-25 05:17:49 +0100

    content

---
+++ src/pages/Content.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Content() {
+  return (
+    <>
+      <div className="bg-emerald-700 text-5xl">
+        Content<h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </div>
+    </>
+  );
+}
+
+export default Content;
